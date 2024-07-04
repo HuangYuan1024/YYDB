e2f86c24325f963776ff3c26d26e63504ec97410
HuangYuan1024 <3205518128@qq.com>
2024-07-04 12:52:00 +0800

    修改rotate功能 2024年7月4日

--- src/main/java/org/yy/service/NormalStore.java
+++ src/main/java/org/yy/service/NormalStore.java
@@ -83,6 +83,7 @@ public class NormalStore implements Store {
             LoggerUtil.info(LOGGER,logFormat, "NormalStore","dataDir isn't exist,creating...");
             file.mkdirs();
         }
+        this.reloadIndex();
         this.RotateDataBaseFile();
     }
 
@@ -90,9 +91,47 @@ public class NormalStore implements Store {
         return this.dataDir + File.separator + NAME + TABLE;
     }
 
+    public String genNewFilePath() {
+        return this.dataDir + File.separator + NAME + "0" + TABLE;
+    }
+
+    public String genScratchFilePath() {
+        return this.dataDir + File.separator + "Scratch.log";
+    }
+
     public void reloadIndex() {
         try {
             RandomAccessFile file = new RandomAccessFile(this.genFilePath(), RW_MODE);
+            long len = file.length();
+            long start = 0;
+            file.seek(start);
+            while (start < len) {
+                int cmdLen = file.readInt();
+                byte[] bytes = new byte[cmdLen];
+                file.read(bytes);
+                JSONObject value = JSON.parseObject(new String(bytes, StandardCharsets.UTF_8));
+                Command command = CommandUtil.jsonToCommand(value);
+                start += 4;
+                if (command != null) {
+                    CommandPos cmdPos = new CommandPos((int) start, cmdLen);
+                    index.put(command.getKey(), cmdPos);
+                    // 如果日志中记载该键值是被删除的，就将其从内存里删去
+                    if (command.getClass().equals(RmCommand.class)) {
+                        index.remove(command.getKey(), cmdPos);
+                    }
+                }
+                start += cmdLen;
+            }
+            file.seek(file.length());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        LoggerUtil.debug(LOGGER, logFormat, "reload index: "+index.toString());
+    }
+
+    public void reloadScratchIndex() {
+        try {
+            RandomAccessFile file = new RandomAccessFile(this.genScratchFilePath(), RW_MODE);
             long len = file.length();
             long start = 0;
             file.seek(start);
@@ -131,8 +170,6 @@ public class NormalStore implements Store {
             if (storeOperateNumber >= storeThreshold) {
                 storeOperateNumber = 0;
 
-//                RotateDataBaseFile();
-
                 RotateDataBaseFile();
             }
             // 写table（wal）文件
@@ -148,11 +185,11 @@ public class NormalStore implements Store {
         } finally {
             indexLock.writeLock().unlock();
         }
-        try {
-            FilCompress(new File(this.genFilePath()));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+//        try {
+//            FilCompress(new File(this.genFilePath()));
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 
     @Override
@@ -165,6 +202,38 @@ public class NormalStore implements Store {
                 return null;
             }
             byte[] commandBytes = RandomAccessFileUtil.readByIndex(this.genFilePath(), cmdPos.getPos(), cmdPos.getLen());
+
+            JSONObject value = null;
+            if (commandBytes != null) {
+                value = JSONObject.parseObject(new String(commandBytes));
+            }
+            Command cmd = null;
+            if (value != null) {
+                cmd = CommandUtil.jsonToCommand(value);
+            }
+            if (cmd instanceof SetCommand) {
+                return ((SetCommand) cmd).getValue();
+            }
+            if (cmd instanceof RmCommand) {
+                return null;
+            }
+        } catch (Throwable t) {
+            throw new RuntimeException(t);
+        } finally {
+            indexLock.readLock().unlock();
+        }
+        return null;
+    }
+
+    public String ReGet(String key) {
+        try {
+            indexLock.readLock().lock();
+            // 从索引中获取信息
+            CommandPos cmdPos = index.get(key);
+            if (cmdPos == null) {
+                return null;
+            }
+            byte[] commandBytes = RandomAccessFileUtil.readByIndex(genScratchFilePath(), cmdPos.getPos(), cmdPos.getLen());
 
             JSONObject value = null;
             if (commandBytes != null) {
@@ -215,19 +284,16 @@ public class NormalStore implements Store {
         } finally {
             indexLock.writeLock().unlock();
         }
-        try {
-            FilCompress(new File(this.genFilePath()));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+//        try {
+//            FilCompress(new File(this.genFilePath()));
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 
     @Override
     public void ReDoLog() {
         reloadIndex();
-
-//                RotateDataBaseFile();
-
         RotateDataBaseFile();
     }
 
@@ -242,61 +308,128 @@ public class NormalStore implements Store {
         }
     }
 
+    public void ScratchLogSet(String key, String value) {
+        try {
+            SetCommand command = new SetCommand(key, value);
+            byte[] commandBytes = command.toByte();
+            // 加锁
+            indexLock.writeLock().lock();
+            // 写table（wal）文件
+            RandomAccessFileUtil.writeInt(genScratchFilePath(), commandBytes.length);
+            RandomAccessFileUtil.write(genScratchFilePath(), commandBytes);
+        } catch (Throwable t) {
+            throw new RuntimeException(t);
+        } finally {
+            indexLock.writeLock().unlock();
+        }
+    }
+
+    public void RenameFile(String oldFileName, String newFileName) {
+        // 假设我们有一个名为"oldName.txt"的文件
+        File oldFile = new File(oldFileName);
+
+        // 我们想要将其重命名为"newName.txt"
+        File newFile = new File(newFileName);
+
+        // 尝试重命名文件
+        boolean success = oldFile.renameTo(newFile);
+
+        // 检查操作是否成功
+        if (success) {
+            System.out.println("文件重命名成功！");
+        } else {
+            System.out.println("文件重命名失败，请确保没有其他程序正在使用该文件。");
+        }
+    }
+
+    public void ReSet(String key, String value) {
+        try {
+            SetCommand command = new SetCommand(key, value);
+            byte[] commandBytes = command.toByte();
+            // 加锁
+            indexLock.writeLock().lock();
+            // 写table（wal）文件
+            RandomAccessFileUtil.writeInt(genFilePath(), commandBytes.length);
+            RandomAccessFileUtil.write(genFilePath(), commandBytes);
+        } catch (Throwable t) {
+            throw new RuntimeException(t);
+        } finally {
+            indexLock.writeLock().unlock();
+        }
+    }
+
     public void CompressLogFile() {
-        ArrayList<String> arrayList = new ArrayList<>();
-        HashSet<String> hashSet = new HashSet<>();
-
-        try (Scanner scanner = new Scanner(new File(this.genFilePath()))) {
-            // 按行读取
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                if (!hashSet.contains(line)) {
-                    arrayList.add(line);
-                    hashSet.add(line);
-                } else if (arrayList.size() > 1){
-                    if (!arrayList.get(arrayList.size() - 1).equals(line)) {
-                        arrayList.add(line);
-                    }
-                }
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        // 索引压缩后写到临时文件
+        for (String key : index.keySet()) {
+            ScratchLogSet(key, Get(key));
         }
 
-        ClearDataBaseFile(this.genFilePath());
+        // 从临时文件加载索引
+        reloadScratchIndex();
 
-        try (FileWriter writer = new FileWriter(this.genFilePath())) {
-            for (String line :
-                arrayList) {
-                writer.write(line + "\r\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        ClearDataBaseFile(genFilePath());
+
+        // 重写日志文件
+        for (String key : index.keySet()) {
+            ReSet(key, ReGet(key));
         }
 
-        index.clear();
+        ClearDataBaseFile(genScratchFilePath());
 
         reloadIndex();
+
+//        ArrayList<String> arrayList = new ArrayList<>();
+//        HashSet<String> hashSet = new HashSet<>();
+//
+//        try (Scanner scanner = new Scanner(new File(this.genFilePath()))) {
+//            // 按行读取
+//            while (scanner.hasNextLine()) {
+//                String line = scanner.nextLine();
+//                if (!hashSet.contains(line)) {
+//                    arrayList.add(line);
+//                    hashSet.add(line);
+//                } else if (arrayList.size() > 1){
+//                    if (!arrayList.get(arrayList.size() - 1).equals(line)) {
+//                        arrayList.add(line);
+//                    }
+//                }
+//            }
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//
+//        ClearDataBaseFile(this.genFilePath());
+//
+//        try (FileWriter writer = new FileWriter(this.genFilePath())) {
+//            for (String line :
+//                arrayList) {
+//                writer.write(line + "\r\n");
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        index.clear();
+//
+//        reloadIndex();
     }
 
     public void RotateDataBaseFile() {
 //        Rotate rotate = new Rotate(normalStore);
 //        rotate.start();
 
-        // 清空数据库文件
-        ClearDataBaseFile("YY-table");
 
         // 压缩日志文件
         CompressLogFile();
 
-        // 重写数据库文件
-        try (FileWriter writer = new FileWriter("YY-table")) {
-            for (String key : index.keySet()) {
-                writer.write(key + "," + Get(key) + "\r\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        // 重写数据库文件
+//        try (FileWriter writer = new FileWriter("Scratch.log")) {
+//            for (String key : index.keySet()) {
+//                writer.write(key + "," + Get(key) + "\r\n");
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 
     public void FilCompress(File file) throws IOException {
